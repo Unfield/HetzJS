4720995307613ccab3fd83af493a94d2cf35615f
Marcel Drinkuth <47082118+Unfield@users.noreply.github.com>
2020-02-25 21:08:12 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const HetzJS = require("./hetz");
-
-HetzJS.authenticate(
-  "ST4C2DAFiyGYGWebrRAoEJkCWzwmGE3d8Wamg28ORkv3z3YjWQv7bf7iaWp4ciHs"
-);
-
-HetzJS.server(4697980).requestConsole(cons => {
-  console.log(cons);
-});
